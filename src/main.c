8f5e893b3f979709d96936623b90c3be1e30671c
rafafelps <rafaelfelipe@alunos.utfpr.edu.br>
2023-05-31 22:01:24 -0300

    Two boxes, fixing alignment

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <raylib.h>
 #include <math.h>
+#include <stdio.h>
 
 #define AMOUNT_BOXES 2
 #define MAX_CIRCLES 100
@@ -15,8 +16,9 @@ struct Circle {
 typedef struct Circle Circle;
 
 void drawEquation();
-Rectangle* initBoxes(unsigned int amountBoxes);
+Rectangle* initBoxes();
 void destroyBoxes(Rectangle* boxes);
+void destroyCircles(Circle** circles);
 
 const unsigned int screenWidth = 800;
 const unsigned int screenHeight = 450;
@@ -24,13 +26,37 @@ int main() {
     InitWindow(screenWidth, screenHeight, "PV = nRT");
     SetTargetFPS(60);
 
-    Rectangle* boxes = initBoxes(AMOUNT_BOXES);
-    Circle* circles = (Circle*)malloc(sizeof(Circle) * MAX_CIRCLES);
-    unsigned int circleCount = 0;
+    Rectangle* boxes = initBoxes();
+    Circle** circles = (Circle**)malloc(sizeof(Circle*) * AMOUNT_BOXES);
+    for (int i = 0; i < AMOUNT_BOXES; i++) {
+        circles[i] = (Circle*)malloc(sizeof(Circle) * MAX_CIRCLES);
+    }
+
+    unsigned int circleCount[2] = {0};
     unsigned int boxThickness = 2;
+
+    for (int currBox = 0; currBox < AMOUNT_BOXES; currBox++) {
+        for (circleCount[currBox] = 0; circleCount[currBox] < MAX_CIRCLES; circleCount[currBox]++) {
+            circles[currBox][circleCount[currBox]].radius = 3;
+            circles[currBox][circleCount[currBox]].color = (Color){0, 71, 171, 255};
+            
+            circles[currBox][circleCount[currBox]].pos.x = GetRandomValue(boxes[currBox].x + boxThickness + circles[currBox][circleCount[currBox]].radius, boxes[currBox].x + boxes[currBox].width - boxThickness - circles[currBox][circleCount[currBox]].radius);
+            circles[currBox][circleCount[currBox]].pos.y = GetRandomValue(boxes[currBox].y + boxThickness + circles[currBox][circleCount[currBox]].radius, boxes[currBox].y + boxes[currBox].height - boxThickness - circles[currBox][circleCount[currBox]].radius);
+
+            float delta = 0;
+            while (delta < 1 || delta > 3) {
+                circles[currBox][circleCount[currBox]].speed.x = (float)GetRandomValue(-180, 180) / 60.f;
+                circles[currBox][circleCount[currBox]].speed.y = (float)GetRandomValue(-180, 180) / 60.f;
+
+                delta = sqrtf(powf(circles[currBox][circleCount[currBox]].speed.x, 2) + powf(circles[currBox][circleCount[currBox]].speed.y, 2));
+            }
+        }
+    }
 
     while (!WindowShouldClose()) {
         // Update
+
+        /* Codigo para adicionar mais bolas
         Vector2 mousePos = GetMousePosition();
         if (mousePos.x > boxes[0].x + boxThickness && mousePos.x < boxes[0].x + boxes[0].width - boxThickness &&
             mousePos.y > boxes[0].y + boxThickness && mousePos.y < boxes[0].y + boxes[0].height - boxThickness) {
@@ -39,7 +65,7 @@ int main() {
                     if (circleCount < MAX_CIRCLES) {
                         circles[circleCount].pos = GetMousePosition();
                         float delta = 0;
-                        while (delta <= 0.5) {
+                        while (delta < 1 || delta > 3) {
                             circles[circleCount].speed.x = (float)GetRandomValue(-180, 180) / 60.f;
                             circles[circleCount].speed.y = (float)GetRandomValue(-180, 180) / 60.f;
 
@@ -52,17 +78,19 @@ int main() {
                     }
                 }
             }
-        }
+        }*/
 
         // Update Circles
-        for (int i = 0; i < circleCount; i++) {
-            circles[i].pos.x += circles[i].speed.x;
-            circles[i].pos.y += circles[i].speed.y;
+        for (int currBox = 0; currBox < AMOUNT_BOXES; currBox++) {
+            for (int i = 0; i < circleCount[currBox]; i++) {
+                circles[currBox][i].pos.x += circles[currBox][i].speed.x;
+                circles[currBox][i].pos.y += circles[currBox][i].speed.y;
 
-            if (circles[i].pos.x - circles[i].radius < boxes[0].x + boxThickness || circles[i].pos.x + circles[i].radius > boxes[0].x + boxes[0].width - boxThickness)
-                circles[i].speed.x *= -1;
-            if (circles[i].pos.y - circles[i].radius < boxes[0].y + boxThickness || circles[i].pos.y + circles[i].radius > boxes[0].y + boxes[0].width - boxThickness)
-                circles[i].speed.y *= -1;
+                if (circles[currBox][i].pos.x - circles[currBox][i].radius < boxes[currBox].x + boxThickness || circles[currBox][i].pos.x + circles[currBox][i].radius > boxes[currBox].x + boxes[currBox].width - boxThickness)
+                    circles[currBox][i].speed.x *= -1;
+                if (circles[currBox][i].pos.y - circles[currBox][i].radius < boxes[currBox].y + boxThickness || circles[currBox][i].pos.y + circles[currBox][i].radius > boxes[currBox].y + boxes[currBox].width - boxThickness)
+                    circles[currBox][i].speed.y *= -1;
+            }
         }
         
         // Render
@@ -73,17 +101,18 @@ int main() {
 
         DrawRectangle(0, 0, screenWidth * 0.3, screenHeight, (Color){189, 181, 213, 255});
 
-        for (int i = 0; i < circleCount; i++) {
-            DrawCircle(circles[i].pos.x, circles[i].pos.y, circles[i].radius, circles[i].color);
-        }
 
         for (int i = 0; i < AMOUNT_BOXES; i++) {
+            for (int j = 0; j < circleCount[i]; j++) {
+                DrawCircle(circles[i][j].pos.x, circles[i][j].pos.y, circles[i][j].radius, circles[i][j].color);
+            }
             DrawRectangleLinesEx(boxes[i], 2, RAYWHITE);
         }
 
         EndDrawing();
     }
 
+    destroyCircles(circles);
     destroyBoxes(boxes);
     CloseWindow();
 
@@ -96,20 +125,28 @@ void drawEquation() {
     DrawRectangle(screenWidth * 0.595, screenHeight * 0.14 + 1, 47, 2, RAYWHITE);
 }
 
-Rectangle* initBoxes(unsigned int amountBoxes) {
-    Rectangle* boxes = (Rectangle*)malloc(sizeof(Rectangle) * amountBoxes);
+Rectangle* initBoxes() {
+    Rectangle* boxes = (Rectangle*)malloc(sizeof(Rectangle) * AMOUNT_BOXES);
 
-    boxes[0].x = screenWidth * 0.4;
-    boxes[0].y = screenHeight * 0.3;
+    Vector2 center = {screenWidth * 0.6, screenHeight * 0.5};
     boxes[0].width = 150;
     boxes[0].height = 150;
+    boxes[0].x = center.x - 25 - (boxes[0].width / 2);
+    boxes[0].y = center.y - (boxes[0].height / 2);
 
     boxes[1] = boxes[0];
-    boxes[1].x += boxes[1].width + 50;
+    boxes[1].x = center.x + 25 + (boxes[0].width / 2);
 
     return boxes;
 }
 
 void destroyBoxes(Rectangle* boxes) {
     free(boxes);
+}
+
+void destroyCircles(Circle** circles) {
+    for (int i = 0; i < AMOUNT_BOXES; i++) {
+        free(circles[i]);
+    }
+    free(circles);
 }
